5f847011e1328ebb5610ab5ab0fe4f3fffd9deeb
arnoldoJose <aolivasgonzales73@gmail.com>
2020-12-16 13:20:21 -0600

    ad axios

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -1,24 +1,22 @@
-import React from 'react'
+import React from 'react';
 import H2Styled from '../Style/H2Styled';
 import H3Styled from '../Style/H3Styled';
 import PStyled from '../Style/PStyled';
 
-const Education = ({datos} ) => (
-  
-    <div className="Education">
-      <H2Styled name="Educacion"/>
-      <div className="Education-container">
-        <div className="Education-item">
-          <H3Styled>
-          <p>UNAN Leon Sede Somoto</p>
-          </H3Styled>
-          <PStyled name="2014-2019"/>
+const Education = ({ datos }) => (
+  <div className="Education">
+    <H2Styled name="Educacion" />
+    <div className="Education-container">
+      <div className="Education-item">
+        <H3Styled>
+          <p>UNAN León Sede Somoto</p>
+        </H3Styled>
+        <PStyled name="2014-2019" />
         <PStyled>{}</PStyled>
-        
-        </div>
       </div>
     </div>
-  )
+  </div>
+);
 
 
 export default Education
--- src/Components/Portafolio.js
+++ src/Components/Portafolio.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import H2Styled from '../Style/H2Styled';
-
+import Axios from 'axios';
 const Portafolio = () => {
+
+  useEffect(() => {
+  let consultarAPI = async () => {
+    let data = await Axios.get(
+      `${process.env.REACT_APP_BACKEND_DELIVERY}/product/all?categoria=food`
+    );
+    console.log(data);
+  };
+  consultarAPI();
+  }, [])
+
   return (
     <div>
       <H2Styled name="Portafolio"/>
